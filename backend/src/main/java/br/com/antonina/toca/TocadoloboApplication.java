6c3d2681c14bedf7f12d534a839567d669657780
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-09-12 16:25:02 -0300

    Backend project created

---
+++ backend/src/main/java/br/com/antonina/toca/TocadoloboApplication.java
@@ -0,0 +1,13 @@
+package br.com.antonina.toca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocadoloboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocadoloboApplication.class, args);
+	}
+
+}
