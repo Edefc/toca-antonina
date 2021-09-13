8b05d3dd2dbe012c0439bc63f6f57d4686feaf6a
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-09-13 12:33:21 -0300

    POST new Category

--- backend/src/main/java/br/com/antonina/toca/resources/CategoryResource.java
+++ backend/src/main/java/br/com/antonina/toca/resources/CategoryResource.java
@@ -1,16 +1,19 @@
 package br.com.antonina.toca.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.antonina.toca.dto.CategoryDTO;
-import br.com.antonina.toca.entities.Category;
 import br.com.antonina.toca.services.CategoryService;
 
 @RestController
@@ -29,14 +32,24 @@ public class CategoryResource {
 		
 		return ResponseEntity.ok().body(list);
 	}
-	
+	/*
+	 * Busca por Id
+	 */
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> finById(@PathVariable Long id) {
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
-		
-		
+	}
+	/*
+	 * Inserção de nova Categoria
+	 */
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		URI uri =ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 		
 	}
+	
 
 }
--- backend/src/main/java/br/com/antonina/toca/services/CategoryService.java
+++ backend/src/main/java/br/com/antonina/toca/services/CategoryService.java
@@ -18,44 +18,39 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
-		List<Category>  list = repository.findAll();
-		
-	
+		List<Category> list = repository.findAll();
+
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		/*List<CategoryDto> listDto = new ArrayList<>();
-		
-		for (Category cat : list) {
-			listDto.add(new CategoryDto(cat));
-		}
-		return listDto;*/
+		/*
+		 * List<CategoryDto> listDto = new ArrayList<>();
+		 * 
+		 * for (Category cat : list) { listDto.add(new CategoryDto(cat)); } return
+		 * listDto;
+		 */
 	}
+
 	/*
 	 * Busca por Id as Categorias
 	 */
+	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundExecption("Categoria não encontrada"));
 		return new CategoryDTO(entity);
 	}
+
+	/*
+	 * Inserção de nova Categoria
+	 */
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
