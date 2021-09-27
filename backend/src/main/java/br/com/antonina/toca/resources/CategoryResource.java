ba2094f1a74eb4902a660397c97a7ba853c64eb8
Edenilson F Chagas <edenilsonfc@gmail.com>
2021-09-27 10:53:01 -0300

    Update Class Category page

--- backend/src/main/java/br/com/antonina/toca/resources/CategoryResource.java
+++ backend/src/main/java/br/com/antonina/toca/resources/CategoryResource.java
@@ -4,8 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -32,7 +30,12 @@ public class CategoryResource {
      * End point busca todas as categorias
      */
 	@GetMapping
-	public ResponseEntity<Page<CategoryDTO>> findAllPaged(
+	public ResponseEntity<Page<CategoryDTO>> findAllPaged (Pageable pageable) {
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(list);
+		
+	}
+	/*public ResponseEntity<Page<CategoryDTO>> findAllPaged(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
@@ -43,7 +46,7 @@ public class CategoryResource {
 		
 		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
-	}
+	}*/
 	
 	
 	/*public ResponseEntity<List<CategoryDTO>> findAll() {
